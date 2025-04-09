7a933ddee5182df41b43ca0a16317fc881f147ad
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-09 20:42:44 +0300

    feat: configure react router & home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex justify-content-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 grow px-4">
+        <Link
+          to={"/people"}
+          className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
+        >
+          🦹 People
+        </Link>
+        <Link
+          to={"/planets"}
+          className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
+        >
+          🪐 Planets
+        </Link>
+        <Link
+          to={"/spaceships"}
+          className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
+        >
+          🚀 Spaceships
+        </Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
