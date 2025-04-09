e4b41cae9a677bf8126cb24fe58daca659656af9
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-09 21:58:01 +0300

    feat: add isr api caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,54 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/swapi\.dev\/api\/people\/\d+\/?$/,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "people-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/swapi\.dev\/api\/starships\/\d+\/?$/,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "spaceships-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/swapi\.dev\/api\/planets\/\d+\/?$/,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "planets-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
 });
