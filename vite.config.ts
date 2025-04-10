26ee04577d18db0e050fc89293e894a08f44d90e
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-10 17:06:35 +0300

    feat: configure pwa assets

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,42 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon-180x180.png",
+        "mascable-icon-512x512.png",
+      ],
+      manifest: {
+        name: "Star Wars PWA",
+        short_name: "SW PWA",
+        description: "A simple demo app to explore the wonders of PWAs",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+
       registerType: "autoUpdate",
       devOptions: { enabled: true },
       workbox: {
