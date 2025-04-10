46d628516df6ff2ce1a66c7f91361b4c9e976ce7
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-10 17:14:35 +0300

    style: add h1 & footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,38 @@ import { Link } from "react-router";
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-content-center items-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 grow px-4">
-        <Link
-          to={"/people"}
-          className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
-        >
-          🦹 People
-        </Link>
-        <Link
-          to={"/planets"}
-          className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
-        >
-          🪐 Planets
-        </Link>
-        <Link
-          to={"/spaceships"}
-          className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
-        >
-          🚀 Spaceships
-        </Link>
+    <div className="min-h-screen flex flex-col">
+      <h1 className="text-3xl font-bold text-center p-6">Star Wars PWA</h1>
+
+      <div className="flex-grow flex justify-center items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-3 w-full px-4">
+          <Link
+            to={"/people"}
+            className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
+          >
+            🦹 People
+          </Link>
+          <Link
+            to={"/planets"}
+            className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
+          >
+            🪐 Planets
+          </Link>
+          <Link
+            to={"/spaceships"}
+            className="border border-gray-300 shadow-md p-4 rounded-md min-h-[200px] flex items-center justify-center text-gray-500 font-medium text-2xl"
+          >
+            🚀 Spaceships
+          </Link>
+        </div>
       </div>
+
+      <footer className="text-center p-4 text-gray-500">
+        powered by{" "}
+        <a href="https://swapi.dev/" className="underline hover:text-gray-700">
+          https://swapi.dev/
+        </a>
+      </footer>
     </div>
   );
 }
