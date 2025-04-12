6f32f26e4d025314ab09cc32d3d4de4a13b15ad7
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-12 11:16:08 +0300

    feat: add base paths for people, spaceships, and planets

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,30 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
+import AppLayout from "./AppLayout.tsx";
 import "./index.css";
 import People from "./People.tsx";
 import Planets from "./Planets.tsx";
+import SearchCharactersContainer from "./SearchCharactersContainer.tsx";
+import SearchPlanetsContainer from "./SearchPlanetsContainer.tsx";
+import SearchSpaceshipsContainer from "./SearchSpaceshipsContainer.tsx";
 import Spaceships from "./Spaceships.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/people/:id" element={<People />} />
-        <Route path="/spaceships/:id" element={<Spaceships />} />
-        <Route path="/planets/:id" element={<Planets />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/people" element={<SearchCharactersContainer />} />
+          <Route path="/people/:id" element={<People />} />
+
+          <Route path="/planets" element={<SearchPlanetsContainer />} />
+          <Route path="/planets/:id" element={<Planets />} />
+
+          <Route path="/spaceships" element={<SearchSpaceshipsContainer />} />
+          <Route path="/spaceships/:id" element={<Spaceships />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
